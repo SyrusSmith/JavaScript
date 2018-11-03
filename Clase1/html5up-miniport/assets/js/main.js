7c7fd40da49ec54fe108551319e6fa71233bd0b0
SyrusSmith <nahueldibiase@hotmail.com>
2018-11-02 21:50:51 -0300

    JavaScript Clase 1

---
+++ Clase1/html5up-miniport/assets/js/main.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+
+    var title = document.getElementById('titular');
+    title.style.display = "none";
+
+    var owl = document.querySelector('#lechuza');
+    owl.style.filter = "grayscale(100%)";
+
+    var reg = document.querySelector('#copyright');
+    console.log(reg);
+
+    var H2 = document.querySelector("h2");
+    H2.style.color = "red";
+}
---
+++ Clase1/js/app.js
@@ -0,0 +1,63 @@
+window.onload = function() {
+    
+    // var nombre = "Nahuel";
+    // var dias = [" Lunes", " Martes", " Miércoles", " Jueves", " Viernes", " Sábado", " Domingo",];
+    // var casado = false;
+
+    // document.write(nombre);
+    // document.write("<br>");
+    // document.write(dias);
+    // document.write("<br>");
+    // document.write(casado);
+    // document.write("<br>");
+
+    // console.log(nombre);
+    // console.log(dias);
+    // console.log(casado);
+
+    // var ironMan = {
+    //     nombre: "Iron Man",
+    //     equipo: "Avengers",
+    //     poderes: ["Volar", "Lanzar Misiles", "Disparar Lásers"],
+    //     energia: 100,
+    //     getPoder: function(i){
+    //         return this.poderes[i]
+    //     }
+    // }
+
+    // while (ironMan.energia>0){
+    //     var randIron = Math.floor(Math.random() * 3);
+    //     if (randIron == 0){
+    //         ironMan.energia = ironMan.energia - 10;
+    //         console.log("Poder: "+ ironMan.getPoder(randIron)+ " - Energía:"+ ironMan.energia);
+    //     }
+    //     if (randIron == 1){
+    //         ironMan.energia = ironMan.energia - 15;
+    //         console.log("Poder: "+ ironMan.getPoder(randIron)+ " - Energía:"+ ironMan.energia);
+    //     }
+    //     if (randIron == 2){
+    //         ironMan.energia = ironMan.energia - 25;
+    //         console.log("Poder: "+ ironMan.getPoder(randIron)+ " - Energía:"+ ironMan.energia);
+    //     }
+    // }
+
+    
+    // var misDatos = {
+    //     nombre: "Nahuel",
+    //     apellido: "Di Biase",
+    //     dni: "33306761",
+    //     comidaFavorita: "Asado",
+    //     edad: "31",
+    //     saludar(){
+    //         return "Mi nombre es "+ this.nombre + " " + this.apellido+ ", y tengo "+ this.edad+ " de edad.";
+    //     }
+    // }
+
+    // for (var key in misDatos){
+    //     console.log(misDatos[key])
+    // }
+
+    // console.log(misDatos.saludar());
+
+    boton.onclick = function(){swal("Testeando el click")}
+}
